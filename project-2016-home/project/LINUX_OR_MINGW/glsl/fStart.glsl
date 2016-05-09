aa0f192b151855a14d64e4e1cb4411e0149f0e4c
feszceri01 <elijah.fetzer@gmail.com>
2016-05-09 22:21:33 +0800

    Part G

--- project-2016-home/project/LINUX_OR_MINGW/glsl/fStart.glsl
+++ project-2016-home/project/LINUX_OR_MINGW/glsl/fStart.glsl
@@ -1,13 +1,48 @@
 #version 150
 
-in  vec4 color;
-in  vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+in vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+in vec3 pos;
+in vec3 normal;
 
 out vec4 fColor;
 
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform vec4 LightPosition;
+uniform float Shininess;
 uniform sampler2D texture;
 
 void main()
 {
+	
+	// The vector to the light from the vertex    
+    vec3 Lvec = LightPosition.xyz - pos;
+
+    // Unit direction vectors for Blinn-Phong shading calculation
+    vec3 L = normalize( Lvec );   // Direction to the light source
+    vec3 E = normalize( -pos );   // Direction to the eye/camera
+    vec3 H = normalize( L + E );  // Halfway vector
+	vec3 N = normalize( normal );	  // Normal vector
+  
+    // Compute terms in the illumination equation
+    vec3 ambient = AmbientProduct;
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct;
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct;
+    
+    if (dot(L, N) < 0.0 ) {
+	specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+    
+    vec4 color;
+    color.rgb = globalAmbient  + ambient + diffuse + specular;
+    color.a = 1.0;
+	
+	
     fColor = color * texture2D( texture, texCoord * 2.0 );
 }
